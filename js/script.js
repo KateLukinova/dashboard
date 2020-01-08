de5c75f7e3f7dc05c91d05ee492d190caf58ca45
Екатерина Лукинова <katyushta@rambler.ru>
2020-01-08 12:17:10 +0300

    popup

--- js/script.js
+++ js/script.js
@@ -2,5 +2,32 @@ $(document).ready(function() {
     $(".menu-icon").click(function () {
         $(this).toggleClass("open");
         $(".sidebar nav").toggleClass("nav-mobile--show");
-    })
+        $(".message-icon").toggleClass("no-visible");
+    });
+    $('.popup-open').click(function() {
+        $('.popup-fade').fadeIn();
+        return false;
+    });
+
+    $('.popup-close').click(function() {
+        $(this).parents('.popup-fade').fadeOut();
+        return false;
+    });
+    $('.popup-close-ok').click(function() {
+        $(this).parents('.popup-fade').fadeOut();
+        return false;
+    });
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            e.stopPropagation();
+            $('.popup-fade').fadeOut();
+        }
+    });
+
+    $('.popup-fade').click(function(e) {
+        if ($(e.target).closest('.popup').length == 0) {
+            $(this).fadeOut();
+        }
+    });
 });
